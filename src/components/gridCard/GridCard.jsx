256da68d152ab2101bdc692e54d50bf9ff260c24
DanFullDev <justbe9523@gmail.com>
2021-11-28 13:26:08 -0500

    Height changes

--- src/components/gridCard/GridCard.jsx
+++ src/components/gridCard/GridCard.jsx
@@ -4,7 +4,7 @@ import SnakeGrid from "../snakeGrid/SnakeGrid";
 
 function GridCard() {
   return (
-    <div className="bg-white border-4 border-gray-400 border-solid m-2 p-5 w-3/6 self-center rounded-lg shadow-xl text-center">
+    <div className="bg-white border-4 border-gray-400 border-solid m-2 p-5 w-2/6 self-center rounded-lg shadow-xl text-center">
       <div>Hello</div>
       <SnakeGrid></SnakeGrid>
     </div>
